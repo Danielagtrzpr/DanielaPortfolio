93d0628f4106e00eafa1cb04e69959a250b8c90b
Danielagtrzpr <danielagp96@gmail.com>
2025-04-19 17:53:50 -0600

    Adding mock images and sketch of About me section responsive

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,54 @@
+import { Binary, BookLock, BugPlay, Code, Code2, Computer, SquareTerminal } from "lucide-react";
+import img11 from "../assets/PlaygroundImage11.jpg";
+
 function AboutMe() {
   return (
-    <section className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-apple-gradient text-4xl">About Me</h2>
+    <section className="relative mt-20 pt-20 px-8 inset-0 w-full flex flex-col justify-center h-screen">
+      <h2 className="pb-8 text-apple-gradient text-4xl mx-auto">About Me</h2>
+      <div className="flex flex-col justify-center text-xs md:m-8 md:gap-8 md:flex-row md:text-base">
+        <div className="flex flex-col flex-1/2 items-center justify-center">
+          <p className="text-justify mt-4 text-gray-500">
+          I'm a passionate frontend developer specializing in React and React Native. 
+          With a strong focus on creating intuitive and performant user interfaces, 
+          I bring designs to life with clean, maintainable code.
+
+          My journey in web development started over 5 years ago, 
+          and I've been working with React ecosystems for most of that time. 
+          I enjoy solving complex UI challenges and creating seamless experiences across different devices.
+          </p>
+        <div className="flex flex-col gap-8 mt-4 sm:flex-row">
+          <div className="border-2 w-full sm:flex-1/2 border-gray-200 rounded-2xl p-8">
+            <h2 className="font-bold text-xl">Skills</h2>
+            <div className="flex flex-col mt-2">
+              <ul className="flex flex-col gap-2">
+                <li className="flex  items-center gap-3"><Code className="text-primary bg-primary/10 p-1 rounded-sm"/><p>React</p></li>
+                <li className="flex  items-center gap-3"><Code2 className="text-primary bg-primary/10 p-1 rounded-sm"/><p>React Native</p></li>
+                <li className="flex  items-center gap-3"><Binary className="text-primary bg-primary/10 p-1 rounded-sm"/><p>JavaScript</p></li>
+                <li className="flex  items-center gap-3"><BugPlay className="text-primary bg-primary/10 p-1 rounded-sm"/><p>TypeScript</p></li>
+                <li className="flex  items-center gap-3"><SquareTerminal className="text-primary bg-primary/10 p-1 rounded-sm"/><p>HTML</p></li>
+                <li className="flex  items-center gap-3"><Computer className="text-primary bg-primary/10 p-1 rounded-sm"/><p>CSS</p></li>
+                <li className="flex  items-center gap-3"><BookLock className="text-primary bg-primary/10 p-1 rounded-sm"/><p>Tailwind CSS</p></li>
+              </ul>
+            </div>
+          </div>
+          <div className="border-2 w-full sm:flex-1/2 border-gray-200  rounded-2xl p-8">
+            <h2 className="font-bold text-xl">Interest</h2>
+            <div className="flex flex-col mt-2">
+              <ul className="flex flex-col gap-2">
+                <li className="flex  items-center gap-3"><Code className="text-primary bg-primary/10 p-1 rounded-sm"/><p>Performance optimization</p></li>
+                <li className="flex  items-center gap-3"><Code2 className="text-primary bg-primary/10 p-1 rounded-sm"/><p>Responsive design</p></li>
+                <li className="flex  items-center gap-3"><Binary className="text-primary bg-primary/10 p-1 rounded-sm"/><p>Cross-platform development</p></li>
+                <li className="flex  items-center gap-3"><BugPlay className="text-primary bg-primary/10 p-1 rounded-sm"/><p>UI/UX design</p></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        </div>
+        <div className="p-8 flex flex-1/2 items-center justify-center">
+          <img src={img11} alt="" className="rounded-4xl" />
+        </div>
+      </div>
     </section>
   );
 }
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,60 +1,71 @@
 import { Project } from "../types";
+import img1 from '../assets/PlaygroundImage1.jpg';
+import img2 from '../assets/PlaygroundImage2.jpg';
+import img3 from '../assets/PlaygroundImage3.jpg';
+import img4 from '../assets/PlaygroundImage4.jpg';
+import img5 from '../assets/PlaygroundImage5.jpg';
+import img6 from '../assets/PlaygroundImage6.jpg';
+import img7 from '../assets/PlaygroundImage7.jpg';
+import img8 from '../assets/PlaygroundImage8.jpg';
+import img9 from '../assets/PlaygroundImage9.jpg';
+import img10 from '../assets/PlaygroundImage10.jpg';
+
 
 export const projects: Project[] = [
   {
     title: 'Project 1',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://pixabay.com/es/photos/cascada-r%C3%ADo-rocas-bosque-monta%C3%B1a-9453143/',
+    image: img1,
   },
   {
     title: 'Project 2',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',                             
+    image: img2,                             
   },
   {
     title: 'Project 3',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',
+    image: img3,  
   },
   {
     title: 'Project 4',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',                             
+    image: img4,                               
   },  {
     title: 'Project 5',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',
+    image: img5,  
   },
   {
     title: 'Project 6',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',                             
+    image: img6,                             
   },  {
     title: 'Project 7',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',
+    image: img7,  
   },
   {
     title: 'Project 8',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',                             
+    image: img8,                             
   },  {
     title: 'Project 9',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',
+    image: img9,  
   },
   {
     title: 'Project 10',
     description: 'Description of project 1.',
     link: '#',
-    image: 'https://via.placeholder.com/150',                             
+    image: img10,                             
   }];
